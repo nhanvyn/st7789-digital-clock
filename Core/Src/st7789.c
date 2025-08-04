a714166b473bea21790a35fb37c016f729fe418e
Vy Nguyen <nhanvyhl1234@gmail.com>
2025-08-04 15:42:38 -0700

    increase the time for clearing LCD buffer to prevent dirty state after MCU is reset

--- Core/Src/st7789.c
+++ Core/Src/st7789.c
@@ -179,9 +179,9 @@ void ST7789_Init(void)
 	#endif
 	HAL_Delay(10);
     ST7789_RST_Clr();
-    HAL_Delay(10);
+    HAL_Delay(200);
     ST7789_RST_Set();
-    HAL_Delay(20);
+    HAL_Delay(200);
 
     ST7789_WriteCommand(ST7789_COLMOD);		//	Set color mode
     ST7789_WriteSmallData(ST7789_COLOR_MODE_16bit);
